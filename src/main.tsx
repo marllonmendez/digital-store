5d55d32d4345831762e25d4e86afa88871c87137
marllonmendez <marlonmendesor@gmail.com>
2024-07-23 19:47:39 -0300

    change: bump version to 1.1.3-dev

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 
 import App from '@/App'
 
-import 'swiper/css'
-import 'swiper/css/pagination'
-
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
